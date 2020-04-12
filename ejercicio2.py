f6a685c97925765d430d00a87574c037a9793353
Orlando Gutierrez <orlandogutierrezlermanda@gmail.com>
2020-04-11 22:39:25 -0400

    ejercicio 2 opraciones aritmeticas

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+print("Calculos para un cuadrado")
+lado=int(input("Ingrese el lado de un cuadrado"))
+area=lado*4
+perimetro=lado**2
+diagonal=lado*(2**(1/2))
+print("El area del cuadrado es : " + str(area))
+print("El perimetro del cuadrado es : " + str(perimetro))
+print("La diagonal del cuadrado es : " + str(diagonal))
