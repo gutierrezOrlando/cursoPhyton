59288c4074cfcb889a8b746e41960ea7eeba7f41
Orlando Gutierrez <orlandogutierrezlermanda@gmail.com>
2020-04-12 13:49:04 -0400

    ejercicio 3 calculo promedio devuelve true

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+prueba1=float(input("Ingresa la nota 1 "))
+prueba2=float(input("Ingresa la nota 2 "))
+prueba3=float(input("Ingresa la nota 3 "))
+examen=float(input("Ingresa la nota del examen: "))
+promedio=(prueba1*0.2+prueba2*0.2+prueba3*0.2+examen*0.4)>=4.0
+print(promedio)
+    
